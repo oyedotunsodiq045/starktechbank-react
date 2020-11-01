6cb5eab1d6b60977a10ff2bd8d482abc1a38b6d4
Sodiq Oyedotun <oyedotunsodiq045@gmail.com>
2020-11-01 07:55:10 +0100

    Footer Redesign

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,8 @@ const Footer = () => {
 		<footer>
 			<Container>
 				<Row>
-					<Col className='text-center py-3'>Copyright &copy; StarkTechBank</Col>
+					{/* <Col className='text-center py-3'>Copyright &copy; StarkTechBank</Col> */}
+					<Col className='text-center py-3'>StarkTechBank built for <a href="https://starktechbank.herokuapp.com/">GTech Inc</a> by Sodiq Oyedotun a.k.a<a href="https://twitter.com/mdo">@major_stark_</a>.</Col>
 				</Row>
 			</Container>
 		</footer>
